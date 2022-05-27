d116e0db7635f8c38f69e6d868ca134c62f26c5d
oppen <arieloppenheimer@outlook.cl>
2022-05-27 15:39:21 -0400

    Hito3

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,5 @@ class Product < ApplicationRecord
     accepts_nested_attributes_for :categories, reject_if: :all_blank, allow_destroy: true
     has_many :m_features, dependent: :destroy
     accepts_nested_attributes_for :m_features, reject_if: :all_blank, allow_destroy: true
+   
 end
