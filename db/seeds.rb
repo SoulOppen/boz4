20051d3ae4ff5e827fb52aec3a1d0db4f75f120b
oppen <arieloppenheimer@outlook.cl>
2022-05-27 14:49:17 -0400

    Modificacion vista f_anidados y Esruuctura seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,29 +8,31 @@
 
 # Ejecutar 'rails db:reset'
 
-require 'faker'
-puts "Poblando Datos..."
+# require 'faker'
+# puts "Poblando Datos..."
 
-45.times do
+# 45.times do
 
-	name = ""
-	cost = Faker::Commerce.price * 1000
-	price = cost * 2
+# 	name = ""
+# 	cost = Faker::Commerce.price * 1000
+# 	price = cost * 2
 	
-	products = Product.create(
-		name: Faker::Commerce.product_name ,
-		desciption: Faker::Lorem.sentences(number: 2),
-		price: price,
-		cost: cost,
-		t_schedule: rand(2..6),
-		stock: rand(1..10),
-		img_url: "https://loremflickr.com/#{rand(50..200)}/#{rand(50..200)}/all",
-		user_id: rand(1..3)
-  	)
-end
-# Esto no es lo mejor solo para efectos de pruebas
-User.create!(email: 'bat@example.com', password: '123456', password_confirmation: '123456', role:'seller')
-User.create!(email: 'super@example.com', password: '123456', password_confirmation: '123456', role:'seller')
-User.create!(email: 'green@example.com', password: '123456', password_confirmation: '123456', role:'seller')
-User.create!(email: 'aqua@example.com', password: '123456', password_confirmation: '123456', role:'buyer') 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# 	products = Product.create(
+# 		name: Faker::Commerce.product_name ,
+# 		desciption: Faker::Lorem.sentences(number: 2),
+# 		price: price,
+# 		cost: cost,
+# 		t_schedule: rand(2..6),
+# 		stock: rand(1..10),
+# 		img_url: "https://loremflickr.com/#{rand(50..200)}/#{rand(50..200)}/all",
+# 		user_id: rand(1..3)
+#   	)
+# end
+# # Esto no es lo mejor solo para efectos de pruebas
+# User.create!(email: 'bat@example.com', password: '123456', password_confirmation: '123456', role:'seller')
+# User.create!(email: 'super@example.com', password: '123456', password_confirmation: '123456', role:'seller')
+# User.create!(email: 'green@example.com', password: '123456', password_confirmation: '123456', role:'seller')
+# User.create!(email: 'aqua@example.com', password: '123456', password_confirmation: '123456', role:'buyer') 
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each { |seed| load seed }
